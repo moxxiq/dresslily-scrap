f16087b013fed0ede63fb8b529ac79d83bf62e87
Moxxiq <dimanavsisto@gmail.com>
2021-08-01 13:34:01 +0300

    Parse link, just to start

--- dresslily_scrapper/dresslily_scrapper/spiders/man_hoodie.py
+++ dresslily_scrapper/dresslily_scrapper/spiders/man_hoodie.py
@@ -4,7 +4,10 @@ import scrapy
 class ManHoodieSpider(scrapy.Spider):
     name = 'man_hoodie'
     allowed_domains = ['https://www.dresslily.com/hoodies-c-181.html']
-    start_urls = ['http://https://www.dresslily.com/hoodies-c-181.html/']
+    start_urls = ['https://www.dresslily.com/hoodies-c-181.html']
 
     def parse(self, response):
-        pass
+        link_to_product = '//a[contains(@class, "goods-name-link")]/@href'
+
+        for link in response.xpath(link_to_product).extract():
+            yield {"url": link}
