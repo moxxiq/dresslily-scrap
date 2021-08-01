fe958a85acfd039a46b2d732b4261a95d32b3194
Moxxiq <dimanavsisto@gmail.com>
2021-08-02 01:48:52 +0300

    Add items dataclasses

--- dresslily_scrapper/dresslily_scrapper/items.py
+++ dresslily_scrapper/dresslily_scrapper/items.py
@@ -1,12 +1,24 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from dataclasses import dataclass
 
 
-class DresslilyScrapperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+@dataclass
+class ProductItem:
+    product_id: int
+    product_url: str
+    name: str
+    discount: int
+    discounted_price: str
+    original_price: str
+    total_reviews: int
+    product_info: str
+
+
+@dataclass
+class ReviewItem:
+    product_id: int
+    rating: int
+    timestamp: float
+    text: str
+    size: str
+    color: str
