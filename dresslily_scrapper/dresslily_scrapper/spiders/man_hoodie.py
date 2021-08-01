75fc238ac935589c046f812cfcbc42f8c1c215dc
Moxxiq <dimanavsisto@gmail.com>
2021-08-01 16:15:32 +0300

    Get links from the pages, follow next page link

--- dresslily_scrapper/dresslily_scrapper/spiders/man_hoodie.py
+++ dresslily_scrapper/dresslily_scrapper/spiders/man_hoodie.py
@@ -3,11 +3,23 @@ import scrapy
 
 class ManHoodieSpider(scrapy.Spider):
     name = 'man_hoodie'
-    allowed_domains = ['https://www.dresslily.com/hoodies-c-181.html']
+    allowed_domains = ['dresslily.com']
     start_urls = ['https://www.dresslily.com/hoodies-c-181.html']
 
     def parse(self, response):
+        """
+        Parse hoodies page. Get all links to the product. Same with rest
+        of the pages.
+        """
+        # XPATH
         link_to_product = '//a[contains(@class, "goods-name-link")]/@href'
+        next_page = '//li/a[.="> "]/@href'
 
-        for link in response.xpath(link_to_product).extract():
-            yield {"url": link}
+        for i, link in enumerate(response.xpath(link_to_product).getall()):
+            yield {
+                "url": link,
+            }
+
+        next_page_url = response.urljoin(response.xpath(next_page).get())
+        if next_page_url is not None:
+            yield response.follow(next_page_url, callback=self.parse)
